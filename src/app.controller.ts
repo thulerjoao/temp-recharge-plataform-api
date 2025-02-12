7857c58fd1fb62a8eabe91ad37c68a943b81affe
thulerjoao <thuler_lima@hotmail.com>
2025-02-12 18:17:44 -0300

    feat: implemented prisma orm and created user and product schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({
+    summary: 'View application status',
+  })
+  getAppStatus(): string {
+    return this.appService.getAppStatus();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
